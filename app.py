b781322bda28d1ea494e70db309d6ce966d26adb
xmadanx <madanmn1742@gmail.com>
2025-08-16 17:24:42 +0530

    Testing

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 monthdict = {1:1,2:4,3:4,
-			 4:0,5:2,6:5,
-			 7:0,8:3,9:6,
-			 10:1,11:4,12:6}
+             4:0,5:2,6:5,
+             7:0,8:3,9:6,
+             10:1,11:4,12:6}
 
 daysdict = {0:"Saturday",
-			1:"Sunday",
-			2:"Monday",
-			3:"Tuesday",
-			4:"Wednesday",
-			5:"Thursday",
-			6:"Friday"}
+            1:"Sunday",
+            2:"Monday",
+            3:"Tuesday",
+            4:"Wednesday",
+            5:"Thursday",
+            6:"Friday"}
 
 day = input("Enter the Day: ")
 month = input("Enter the Month: ")
 year = input("Enter the Year: ")
 
 def result():
-	century = 6 - (2 * (int(year[:2]) % 4)) 
-	yearnum = int(year[2:])
-	yearcalc = yearnum // 4
-	if (yearnum % 4 == 0) and (int(month) < 3):
-		leap = 1
-	else:
-		leap = 0
-	result = int(day) + monthdict[int(month)]
-	result += century + yearnum + yearcalc - leap
-	print(daysdict[result%7])
+    century = 6 - (2 * (int(year[:2]) % 4))
+    print(f"Century: {century}")
+    yearnum = int(year[2:])
+    print(f"Year: {yearnum}")
+    yearcalc = yearnum // 4
+    print(f"Yearcalc: {yearcalc}")
+    if (yearnum % 4 == 0) and (int(month) < 3):
+        leap = 1
+    else:
+        leap = 0
+    result = int(day) + monthdict[int(month)]
+    result += century + yearnum + yearcalc - leap
+    print(f"Result: {result}")
+    print(daysdict[result%7])
 
 if __name__ == "__main__":
-	result()
+    result()
